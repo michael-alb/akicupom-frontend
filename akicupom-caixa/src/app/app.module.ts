9a69dc09f02dbaafc6348345d09dbbdb4a0b7aa6
michael.alb <maikonmorais@gmail.com>
2017-09-18 15:28:25 -0300

    Refatoração dashboard

--- akicupom-caixa/src/app/app.module.ts
+++ akicupom-caixa/src/app/app.module.ts
@@ -7,18 +7,18 @@ import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { routing } from './app.routing';
-import { promocoesRouting } from "./promocoes/promocoes.routing";
-import { PromocoesModule } from "./promocoes/promocoes.module";
+import { promocoesRouting } from './promocoes/promocoes.routing';
+import { PromocoesModule } from './promocoes/promocoes.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent,
+    DashboardComponent,
     NotFoundComponent,
     LoginComponent
   ],
