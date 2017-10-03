82babb5154c6ecfc8fa7cbd9848f33f21e4f4186
michael.alb <maikonmorais@gmail.com>
2017-10-03 11:56:49 -0300

    Reformulação da Home

--- akicupom-home/src/app/promocoes/shared/promocao.ts
+++ akicupom-home/src/app/promocoes/shared/promocao.ts
@@ -1,6 +1,11 @@
 export class Promocao {
   id: number;
+  capa: string;
   nome: string;
   descricao: string;
   dataValidade: string;
+  status: boolean;
+  valor_promocao: number;
+  promo_cat: number;
+  fornecedor_id: number;
 }
